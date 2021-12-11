5c63009d367f3224d49f4e675280d413b23cec60
Ivan Imbert <ivan.imbert@epita.fr>
2021-12-11 19:02:31 +0100

    Adding the arch

---
+++ builtin.py
@@ -0,0 +1,402 @@
+
+def abs() :
+    """
+    Returns the absolute value of a number
+    """
+    pass
+
+def all() :
+    """
+    Returns True if all items in an iterable object are true
+    """
+    pass
+
+def any() :
+    """
+    Returns True if any item in an iterable object is true
+    """
+    pass
+
+def ascii() :
+    """
+    Returns a readable version of an object. Replaces none-ascii characters with escape character
+    """
+    pass
+
+def bin() :
+    """
+    Returns the binary version of a number
+    """
+    pass
+
+def bool() :
+    """
+    Returns the boolean value of the specified object
+    """
+    pass
+
+def bytearray() :
+    """
+    Returns an array of bytes
+    """
+    pass
+
+def bytes() :
+    """
+    Returns a bytes object
+    """
+    pass
+
+def callable() :
+    """
+    Returns True if the specified object is callable, otherwise False
+    """
+    pass
+
+def chr() :
+    """
+    Returns a character from the specified Unicode code.
+    """
+    pass
+
+def classmethod() :
+    """
+    Converts a method into a class method
+    """
+    pass
+
+def compile() :
+    """
+    Returns the specified source as an object, ready to be executed
+    """
+    pass
+
+def complexe() :
+    """
+    Returns a complex number
+    """
+    pass
+
+def delattr() :
+    """
+    Deletes the specified attribute (property or method) from the specified object
+    """
+    pass
+
+def dict() :
+    """
+    Returns a dictionary (Array)
+    """
+    pass
+
+def dir() :
+    """
+    Returns a list of the specified object's properties and methods
+    """
+    pass
+
+def divmod() :
+    """
+    Returns the quotient and the remainder when argument1 is divided by argument2
+    """
+    pass
+
+def enumerate() :
+    """
+    Takes a collection (e.g. a tuple) and returns it as an enumerate object
+    """
+    pass
+
+def eval() :
+    """
+    Evaluates and executes an expression
+    """
+    pass
+
+def exec() :
+    """
+    Executes the specified code (or object)
+    """
+    pass
+
+def filter() :
+    """
+    Use a filter function to exclude items in an iterable object
+    """
+    pass
+
+def float() :
+    """
+    Returns a floating point number
+    """
+    pass
+
+def format() :
+    """
+    Formats a specified value
+    """
+    pass
+
+def frozenset() :
+    """
+    Returns a frozenset object
+    """
+    pass
+
+def getattr() :
+    """
+    Returns the value of the specified attribute (property or method)
+    """
+    pass
+
+def globals() :
+    """
+    Returns the current global symbol table as a dictionary
+    """
+    pass
+
+def hasattr() :
+    """
+    Returns True if the specified object has the specified attribute (property/method)
+    """
+    pass
+
+def hash() :
+    """
+    Returns the hash value of a specified object
+    """
+    pass
+
+def help() :
+    """
+    Executes the built-in help system
+    """
+    pass
+
+def hex() :
+    """
+    Converts a number into a hexadecimal value
+    """
+    pass
+
+def id() :
+    """
+    Returns the id of an object
+    """
+    pass
+
+def input() :
+    """
+    Allowing user input
+    """
+    pass
+
+def int() :
+    """
+    Returns an integer number
+    """
+    pass
+
+def isinstance() :
+    """
+    Returns True if a specified object is an instance of a specified object
+    """
+    pass
+
+def issubclass() :
+    """
+    Returns True if a specified class is a subclass of a specified object
+    """
+    pass
+
+def iter() :
+    """
+    Returns an iterator object
+    """
+    pass
+
+def len() :
+    """
+    Returns the length of an object
+    """
+    pass
+
+def list() :
+    """
+    Returns a list
+    """
+    pass
+
+def locals() :
+    """
+    Returns an updated dictionary of the current local symbol table
+    """
+    pass
+
+def map() :
+    """
+    Returns the specified iterator with the specified function applied to each item
+    """
+    pass
+
+def max() :
+    """
+    Returns the largest item in an iterable
+    """
+    pass
+
+def memoryview() :
+    """
+    Returns a memory view object
+    """
+    pass
+
+def min() :
+    """
+    Returns the smallest item in an iterable
+    """
+    pass
+
+def next() :
+    """
+    Returns the next item in an iterable
+    """
+    pass
+
+def object() :
+    """
+    Returns a new object
+    """
+    pass
+
+def oct() :
+    """
+    Converts a number into an octal
+    """
+    pass
+
+def open() :
+    """
+    Opens a file and returns a file object
+    """
+    pass
+
+def ord() :
+    """
+    Convert an integer representing the Unicode of the specified character
+    """
+    pass
+
+def pow() :
+    """
+    Returns the value of x to the power of y
+    """
+    pass
+
+def print() :
+    """
+    Prints to the standard output device
+    """
+    pass
+
+def property() :
+    """
+    Gets, sets, deletes a property
+    """
+    pass
+
+def range() :
+    """
+    Returns a sequence of numbers, starting from 0 and increments by 1 (by default)
+    """
+    pass
+
+def repr() :
+    """
+    Returns a readable version of an object
+    """
+    pass
+
+def reversed() :
+    """
+    Returns a reversed iterator
+    """
+    pass
+
+def round() :
+    """
+    Rounds a numbers
+    """
+    pass
+
+def set() :
+    """
+    Returns a new set object
+    """
+    pass
+
+def setattr() :
+    """
+    Sets an attribute (property/method) of an object
+    """
+    pass
+
+def slice() :
+    """
+    Returns a slice object
+    """
+    pass
+
+def sorted() :
+    """
+    Returns a sorted list
+    """
+    pass
+
+def staticmethod() :
+    """
+    Converts a method into a static method
+    """
+    pass
+
+def str() :
+    """
+    Returns a string object
+    """
+    pass
+
+def sum() :
+    """
+    Sums the items of an iterator
+    """
+    pass
+
+def super() :
+    """
+    Returns an object that represents the parent class
+    """
+    pass
+
+def tuple() :
+    """
+    Returns a tuple
+    """
+    pass
+
+def type() :
+    """
+    Returns the type of an object
+    """
+    pass
+
+def vars() :
+    """
+    Returns the __dict__ property of an object
+    """
+    pass
+
+def zip() :
+    """
+    Returns an iterator, from two or more iterators
+    """
+    pass
---
+++ strings.py
@@ -0,0 +1,264 @@
+def capitalize()	Converts the first character to upper case
+def casefold() :
+    """
+	Converts string into lower case
+    """
+    pass
+
+def center():
+    """
+    Returns a centered string
+    """
+    pass
+
+def count():
+    """
+    Returns the number of times a specified value occurs in a string
+    """
+    pass
+
+def encode():
+    """
+    Returns an encoded version of the string
+    """
+    pass
+
+def endswith():
+    """
+    Returns true if the string ends with the specified value
+    """
+    pass
+
+def expandtabs():
+    """
+    Sets the tab size of the string
+    """
+    pass
+
+def find():
+    """
+    Searches the string for a specified value and returns the position of where it was found
+    """
+    pass
+
+def format():
+    """
+    Formats specified values in a string
+    """
+    pass
+
+def format_map():
+    """
+    Formats specified values in a string
+    """
+    pass
+
+def index():
+    """
+    Searches the string for a specified value and returns the position of where it was found
+    """
+    pass
+
+def isalnum():
+    """
+    Returns True if all characters in the string are alphanumeric
+    """
+    pass
+
+def isalpha():
+    """
+    Returns True if all characters in the string are in the alphabet
+    """
+    pass
+
+def isascii():
+    """
+    Returns True if all characters in the string are ascii characters
+    """
+    pass
+
+def isdecimal():
+    """
+    Returns True if all characters in the string are decimals
+    """
+    pass
+
+def isdigit():
+    """
+    Returns True if all characters in the string are digits
+    """
+    pass
+
+def isidentifier():
+    """
+    Returns True if the string is an identifier
+    """
+    pass
+
+def islower():
+    """
+    Returns True if all characters in the string are lower case
+    """
+    pass
+
+def isnumeric():
+    """
+    Returns True if all characters in the string are numeric
+    """
+    pass
+
+def isprintable():
+    """
+    Returns True if all characters in the string are printable
+    """
+    pass
+
+def isspace():
+    """
+    Returns True if all characters in the string are whitespaces
+    """
+    pass
+
+def istitle():
+    """
+    Returns True if the string follows the rules of a title
+    """
+    pass
+
+def isupper():
+    """
+    Returns True if all characters in the string are upper case
+    """
+    pass
+
+def join():
+    """
+    Converts the elements of an iterable into a string
+    """
+    pass
+
+def ljust():
+    """
+    Returns a left justified version of the string
+    """
+    pass
+
+def lower():
+    """
+    Converts a string into lower case
+    """
+    pass
+
+def lstrip():
+    """
+    Returns a left trim version of the string
+    """
+    pass
+
+def maketrans():
+    """
+    Returns a translation table to be used in translations
+    """
+    pass
+
+def partition():
+    """
+    Returns a tuple where the string is parted into three parts
+    """
+    pass
+
+def replace():
+    """
+    Returns a string where a specified value is replaced with a specified value
+    """
+    pass
+
+def rfind():
+    """
+    Searches the string for a specified value and returns the last position of where it was found
+    """
+    pass
+
+def rindex():
+    """
+    Searches the string for a specified value and returns the last position of where it was found
+    """
+    pass
+
+def rjust():
+    """
+    Returns a right justified version of the string
+    """
+    pass
+
+def rpartition():
+    """
+    Returns a tuple where the string is parted into three parts
+    """
+    pass
+
+def rsplit():
+    """
+    Splits the string at the specified separator, and returns a list
+    """
+    pass
+
+def rstrip():
+    """
+    Returns a right trim version of the string
+    """
+    pass
+
+def split():
+    """
+    Splits the string at the specified separator, and returns a list
+    """
+    pass
+
+def splitlines():
+    """
+    Splits the string at line breaks and returns a list
+    """
+    pass
+
+def startswith():
+    """
+    Returns true if the string starts with the specified value
+    """
+    pass
+
+def strip():
+    """
+    Returns a trimmed version of the string
+    """
+    pass
+
+def swapcase():
+    """
+    Swaps cases, lower case becomes upper case and vice versa
+    """
+    pass
+
+def title():
+    """
+    Converts the first character of each word to upper case
+    """
+    pass
+
+def translate():
+    """
+    Returns a translated string
+    """
+    pass
+
+def upper():
+    """
+    Converts a string into upper case
+    """
+    pass
+
+def zfill():
+    """
+    Fills the string with a specified number of 0 values at the beginning
+    """
+    pass
